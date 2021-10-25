53059e17eab26db6717cf75114ddbe0d2b40817e
James Anderson <james.anderson4@coxautoinc.com>
2021-10-25 17:33:30 -0400

    initial

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+  User.create([{ first_name: 'John', last_name: 'Doe' }, { first_name: 'Jane', last_name: 'Doe' }])
+#   Character.create(name: 'Luke', movie: movies.first)
